47b38522e31a04e7c0af375b4ae38187711c4b34
shinyui <shinyuichu325@gmail.com>
2024-04-30 21:18:34 +0800

    update siteurl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nextjs-tailwind-contentlayer-blog-starter.vercel.app',
+  siteUrl: 'https://shinyui.xyz',
   generateRobotsTxt: true,
 };
